db9414f494050dafc1781cefccf8739814e850fe
GwenaelGirard <gwenael.girard@divtec.ch>
2022-02-18 09:51:42 +0100

    Maquette terminée et fonctionnelle

--- app/src/main/java/com/giragwe/speedquiz/Controllers/QuestionManager.java
+++ app/src/main/java/com/giragwe/speedquiz/Controllers/QuestionManager.java
@@ -1,5 +1,20 @@
 package com.giragwe.speedquiz.Controllers;
 
+import com.giragwe.speedquiz.Models.Question;
+
+import java.util.ArrayList;
+
 public class QuestionManager {
+    public ArrayList<Question> createQuestion() {
+        ArrayList<Question> questionsList = new ArrayList<>();
+
+        questionsList.add(new Question("Le Java est une language de programmation", true));
+        questionsList.add(new Question("Le Javascript est un cousin du Java", false));
+        questionsList.add(new Question("Le CSS est un language de programmation", false));
+        questionsList.add(new Question("Le HTML est un langage de balisage", true));
+
+        return questionsList;
+    }
+
 
 }
--- app/src/main/java/com/giragwe/speedquiz/activity_game.java
+++ app/src/main/java/com/giragwe/speedquiz/activity_game.java
@@ -13,9 +13,27 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+class Question {
+    private String question;
+    private Boolean reponse;
+
+    public Question(String question, Boolean reponse) {
+        this.question = question;
+        this.reponse = reponse;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public Boolean getReponse() {
+        return reponse;
+    }
+}
+
 enum Joueurs {
     JOUEUR1,
-    JOUEUR2;
+    JOUEUR2
 }
 
 
